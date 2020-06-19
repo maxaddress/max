8c3e3d762f4b392edd7e5dae4fa5ffd728eff9e4
Max Lima <42284123+maxaddress@users.noreply.github.com>
2020-06-19 15:38:46 -0300

    Update telnetlib.py

--- telnetlib.py
+++ telnetlib.py
@@ -1,17 +1,17 @@
 import getpass
 import telnetlib
 
-HOST = "192.168.122.71" # variável recebe endereço IP do R1
+HOST = "192.168.122.71" # variável recebendo o endereço IP do R1
 user = input("Enter your telnet Username: ") # customizando o texto padrão (opcional)  
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST) # usando a biblioteca telnetlib para "telnetting" o IP contido em "HOST"
 
-tn.read_until(b"Username: ") # mudar do padrão "login" para "Username"
+tn.read_until(b"Username: ") # mudar do padrão "login" para "Username" conforme Cisco IOS
 tn.write(user.encode('ascii') + b"\n") # insere o usuário digitado na variável "user" 
 if password:
 	tn.read_until(b"Password: ") # vai esperar a senha da variável "Password"
-	tn.write(password.encode('ascii') + b"\n") # escreve senha contida na variável "Password"
+	tn.write(password.encode('ascii') + b"\n") # "escreve" senha contida na variável "Password"
 
 tn.write(b"enable\n") # "escreve" o comando enable no router
 tn.write(b"cisco\n") # "escreve" a senha de enable configurada antes
